7713facf187b4af8784f38218cca56d1f463b506
Jan Gerber <j@thing.net>
2012-07-24 03:31:45 +0000

    Add Special:TimedMediaHandler
    special page with some statistics and currently running jobs
    
    Change-Id: Ifafa486efdcd06113923e7789e67a49ce4573106

--- TimedMediaHandler.i18n.php
+++ TimedMediaHandler.i18n.php
@@ -100,6 +100,7 @@ Are you sure you want to proceed?',
 	'timedmedia-subtitle-language' => '$1 ($2) subtitles',
 	'timedmedia-subtitle-no-video' => 'There is no video associated with the current subtitle page',
 	'timedmedia-subtitle-no-subtitles' => 'There are currently no subtitles in $1 for this video, you can [{{fullurl:{{FULLPAGENAME}}|action=edit}} edit this page] to add them',
+	'timedmediahandler'             => 'TimedMediaHandler',
 );
 
 /** Message documentation (Message documentation)
