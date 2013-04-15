aff182289a9d9041bea4b2e47d87e20bb36503a6
Jan Gerber <jgerber@wikimedia.org>
2013-04-15 22:49:37 +0000

    deduplicate transcode jobs
    deduplicate transcode jobs on title/key and
    add maintenance script to resubmit
    unprocessed transcode jobs
    
    Change-Id: Ibb4f7fff37cefaa784a9d5577fa31db034363d81

---
+++ maintenance/resetTranscodes.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * reset stalled transcodes
+ *
+ */
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = dirname( __FILE__ ) . '/../../..';
+}
+require_once( "$IP/maintenance/Maintenance.php" );
+
+class ResetTranscodes extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = "reset stalled transcodes, that are no longer in job queue.";
+	}
+	public function execute() {
+		global $wgEnabledTranscodeSet;
+		$where = array(
+			"transcode_time_startwork" => NULL,
+			"transcode_time_error" => NULL
+		);
+		$dbr = wfGetDB( DB_SLAVE );
+		$res = $dbr->select( 'transcode', '*', $where, __METHOD__ );
+		foreach ( $res as $row ) {
+			$title = Title::newFromText( $row->transcode_image_name, NS_FILE );
+			$transcodeKey= $row->transcode_key;
+			// re-insert WebVideoTranscodeJob,
+			// will only be added if not in queue
+			// due to deduplication
+			$job = new WebVideoTranscodeJob( $title, array(
+				'transcodeMode' => 'derivative',
+				'transcodeKey' => $transcodeKey,
+			) );
+			$jobId = $job->insert();
+		}
+	}
+}
+
+$maintClass = 'ResetTranscodes'; // Tells it to run the class
+require_once( RUN_MAINTENANCE_IF_MAIN );
