44fd747b52a6e703d396e52a444227d01a51657c
Jan Gerber <jgerber@wikimedia.org>
2013-04-19 13:11:04 +0000

    add maintenance script to retry failed transcodes
    i.e to retry all transcodes that failed in
    av_interleaved_write_frame before it this
    was fixed with libvpx update:
    
    php retryTranscodes.php \
     --error "av_interleaved_write_frame(): Invalid argument"
    
    Bug: 46795
    Change-Id: I41b8d905716423da513d16074de3b388eae05cce

---
+++ maintenance/retryTranscodes.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * retry transcodes no longer defined in $wgEnabledTranscodeSet
+ *
+ */
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = dirname( __FILE__ ) . '/../../..';
+}
+require_once( "$IP/maintenance/Maintenance.php" );
+
+class RetryTranscodes extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->addOption( "key", "retry all failed transcodes for given key", false, true );
+		$this->addOption( "error", "retry all failed transcodes matching error substring", false, true);
+		$this->mDescription = "retry transcodes for given key or error";
+	}
+	public function execute() {
+		if ( !$this->hasOption( "error" ) && !$this->hasOption( "key" ) ) {
+			$this->output("You have to provide --key and/or --error\n");
+			return;
+		}
+		$dbw = wfGetDB( DB_MASTER );
+		$cond = array();
+		$cond[] = 'transcode_time_error IS NOT NULL';
+		if ( $this->hasOption( "key" ) ) {
+			$cond['transcode_key'] = $this->getOption( 'key' );
+		}
+		if ( $this->hasOption( "error" ) ) {
+			$cond[] = "transcode_error " . $dbw->buildLike( $dbw->anyString(),
+				$this->getOption( 'error' ), $dbw->anyString() );
+		}
+		$dbw->delete( 'transcode', $cond, __METHOD__ );
+	}
+}
+
+$maintClass = 'RetryTranscodes'; // Tells it to run the class
+require_once( RUN_MAINTENANCE_IF_MAIN );
