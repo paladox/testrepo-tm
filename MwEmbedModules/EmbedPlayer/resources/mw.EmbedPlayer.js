68de3290609a65b93777c198b7fd66f2474d7abc
Jan Gerber <jgerber@wikimedia.org>
2013-05-22 14:11:56 +0000

    disablecontrols should only be set once not per source
    Change-Id: I23406ba236ee869081b1bda4ce37706b44b9ba93

--- MwEmbedModules/EmbedPlayer/resources/mw.EmbedPlayer.js
+++ MwEmbedModules/EmbedPlayer/resources/mw.EmbedPlayer.js
@@ -81,6 +81,9 @@
 		// can be displayed in the player without loading the media file
 		'data-durationhint': null,
 
+		// to disable menu or timedText for a given embed
+		'data-disablecontrols': null,
+
 		// Also support direct durationHint attribute ( backwards compatibly )
 		// @deprecated please use data-durationhint instead.
 		'durationHint' : null,
@@ -342,6 +345,11 @@
 			// Make sure duration is a float:
 			this.duration = parseFloat( this.duration );
 			mw.log( 'EmbedPlayer::init:' + this.id + " duration is: " + this.duration );
+
+			// Add disablecontrols property form data-disablecontrols:
+			if( _this['data-disablecontrols'] ){
+				_this.disablecontrols = _this['data-disablecontrols'];
+			}
 
 			// Set the playerElementId id
 			this.pid = 'pid_' + this.id;
