a3eb792a64e5eb918b219bff8f1acfa2a84e1779
Brian Wolff <bawolff+wn@gmail.com>
2013-06-19 02:02:23 -0300

    [Goes with I33462a8] Support for new gallery.
    This goes with I33462a8. It adds support for new
    gallery code, by provided tmh specific js for
    resizing videos, and for embedding higher than
    neccesary resolution media. See I33462a8
    for the justification
    
    Change-Id: Ie82c1548b1e6ec048cdac8a3a51b6163b844a242

---
+++ resources/mw.TMHGalleryHook.js
@@ -0,0 +1,36 @@
+/**
+* Simple script to add pop-up video dialog link support for video thumbnails
+*/
+( function( mw, $ ) {
+	// Hook to allow dynamically resizing videos in image galleries
+	mw.hook( 'mediawiki.page.gallery.resize' ).add( function( info ) {
+		var $mwContainer = info.$imageDiv.find( '.mediaContainer' ),
+		$mwPlayerContainer,
+		$tmhVideo;
+		if ( info.resolved ) {
+			// Everything is already done here.
+			return;
+		}
+
+		$mwContainer = info.$imageDiv.find( '.mediaContainer' );
+		if ( $mwContainer.length ) {
+			// Add some padding, so caption doesn't overlap video controls.
+			info.$outerDiv.find( 'div.gallerytext' ).css( 'padding-bottom', '20px' );
+
+			info.$imageDiv.width( info.imgWidth );
+			$mwContainer.width( info.imgWidth );
+			$mwPlayerContainer = $mwContainer.children( '.mwPlayerContainer' );
+			if ( $mwPlayerContainer.length ) {
+				// Case 1: HTML5 player already loaded
+				$mwPlayerContainer.width( info.imgWidth ).height( info.imgHeight );
+				$mwPlayerContainer.find( 'img.playerPoster' ).width( info.imgWidth ).height( info.imgHeight );
+			} else {
+				// Case 2: Raw video element
+				$tmhVideo = info.$imageDiv.find( 'video' );
+				$tmhVideo.width( info.imgWidth ).height( info.imgHeight );
+			}
+			info.resolved = true;
+			return;
+		}
+	});
+} )( mediaWiki, jQuery );
