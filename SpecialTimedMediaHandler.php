d8f1ed38d3ec66de3e0cd7d7975643697d6a934a
Brian Wolff <bawolff+wn@gmail.com>
2013-11-27 17:27:36 -0400

    Do language formatting on numbers in [[Special:TimedMediaHandler]]
    At some future point, this should maybe have i18n done as a
    parameter to message, instead of just outputting the number,
    but that's kind of a separate issue.
    
    Change-Id: I42a711f100f28be5e3914f6ab5d52fa9f1b3f0b9

--- SpecialTimedMediaHandler.php
+++ SpecialTimedMediaHandler.php
@@ -69,13 +69,13 @@ class SpecialTimedMediaHandler extends SpecialPage {
 			// timedmedia-derivative-state-queued, timedmedia-derivative-state-failed
 			$out->addHTML(
 				"<h2>"
-				. $this->msg( 'timedmedia-derivative-state-' . $state, $states[ $state ]['total'] )->escaped()
+				. $this->msg( 'timedmedia-derivative-state-' . $state )->numParams( $states[ $state ]['total'] )->escaped()
 				. "</h2>"
 			);
 			foreach( $allTranscodes as $key ) {
 				if ( $states[ $state ][ $key ] ) {
 					$out->addHTML(
-						$states[ $state ][ $key ]
+						htmlspecialchars( $this->getLanguage()->formatNum( $states[ $state ][ $key ] ) )
 						. ' '
 						. $this->msg( 'timedmedia-derivative-desc-' . $key )->escaped()
 						. "<br>" );
