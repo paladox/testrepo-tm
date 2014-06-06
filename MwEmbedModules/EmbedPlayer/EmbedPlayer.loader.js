d0f52b8b5adbbe1f87acd12e28f073a2ee16405c
Gilles Dubuc <gdubuc@wikimedia.org>
2014-06-06 11:57:51 +0200

    Fix jquery.client dependencies
    Change-Id: Ia07c5b885107b8b2a42de2a6c21c31512ce5dbb3
    Mingle: https://wikimedia.mingle.thoughtworks.com/projects/multimedia/cards/592

--- MwEmbedModules/EmbedPlayer/EmbedPlayer.loader.js
+++ MwEmbedModules/EmbedPlayer/EmbedPlayer.loader.js
@@ -2,33 +2,35 @@
 * EmbedPlayer loader
 */
 ( function( mw, $ ) {
-
 	/**
 	* Add a DOM ready check for player tags
 	*/
-	$(function( event ){
+	$( function() {
 		var $selected = $( mw.config.get( 'EmbedPlayer.RewriteSelector' ) );
-		if( $selected.length ){
+		if ( $selected.length ) {
 			var inx = 0;
-			var checkSetDone = function(){
-				if( inx < $selected.length ){
+			var checkSetDone = function() {
+				if ( inx < $selected.length ) {
 					// put in timeout to avoid browser lockup, and function stack
-					$selected.slice(inx, inx+1).embedPlayer( function(){
-						setTimeout(function(){
+					$selected.slice( inx, inx + 1 ).embedPlayer( function() {
+						setTimeout( function() {
 							checkSetDone();
-						}, 5);
-					});
+						}, 5 );
+					} );
 				}
 				inx++;
 			};
-			checkSetDone();
+
+			mw.loader.using( 'jquery.client', function() {
+				checkSetDone();
+			} );
 		}
-	});
+	} );
 
 	/**
 	* Add the mwEmbed jQuery loader wrapper
 	*/
-	$.fn.embedPlayer = function( readyCallback ){
+	$.fn.embedPlayer = function( readyCallback ) {
 		var playerSet = this;
 		mw.log( 'jQuery.fn.embedPlayer :: ' + $( this ).length );
 
@@ -37,15 +39,12 @@
 			'mw.EmbedPlayer'
 		];
 
-		var rewriteElementCount = 0;
-		$( this ).each( function(inx, playerElement){
-			var skinName = '',
-				profile = $.client.profile();
-
+		$( this ).each( function( inx, playerElement ) {
 			// we have javascript ( disable controls )
 			$( playerElement ).removeAttr( 'controls' );
 			// Add an overlay loader ( firefox has its own native loading spinner )
-			if ( profile.name !== 'firefox' ){
+
+			if ( $.client.profile().name !== 'firefox' ) {
 				$( playerElement )
 					.parent()
 					.getAbsoluteOverlaySpinner()
@@ -54,19 +53,17 @@
 			// Allow other modules update the dependencies
 			$( mw ).trigger( 'EmbedPlayerUpdateDependencies',
 					[ playerElement, dependencySet ] );
-		});
+		} );
 
 		// Remove any duplicates in the dependencySet:
 		dependencySet = $.uniqueArray( dependencySet );
 
 		// Do the request and process the playerElements with updated dependency set
-		mediaWiki.loader.using( dependencySet, function(){
+		mw.loader.using( dependencySet, function() {
 			// Setup the enhanced language:
 			mw.processEmbedPlayers( playerSet, readyCallback );
-		}, function( e ){
-			throw new Error( 'Error loading EmbedPlayer dependency set: ' + e.message  );
-		});
+		}, function( e ) {
+			throw new Error( 'Error loading EmbedPlayer dependency set: ' + e.message );
+		} );
 	};
-
-
 } )( window.mediaWiki, window.jQuery );
