b9a42677f62cfec80ec334fc2e14a0caeba1a47b
Brion Vibber <brion@pobox.com>
2014-07-12 00:24:34 -0700

    Work in progress: early testing of integration with ogv.js media player
    ogv.js is a basic audio/video player in JavaScript, using the standard
    Xiph ogg/vorbis/theora libraries cross-compiled with emscripten.
    
    It's meant to provide Ogg media playback in Safari and Internet Explorer
    when native decoders are not installed (or cannot be installed, as on
    iOS and Windows RT).
    
    Note that a thin Flash audio output shim is used on IE 10/11;
    Safari and other modern browsers use the standard W3C Web Audio API
    where available.
    
    See source and live examples via https://github.com/brion/ogv.js
    
    Issues:
    * need to add credits/license info for ogg, vorbis, theora, opus, ogv.js
    * oddities with the popup player (multiple play buttons?)
    * transcodes don't play right on my test box, not sure if transcodes
      are broken or the player...
    * audio doesn't work on iOS Safari yet, as the audio must be started
      from an event handler but it's very async here.
    * ogvjs.js file is not optimized for size -- it's about 1MB uncompressed,
      gzipping down to something like 250k-ish.
    * Currently depends on server configuration to do gzipping -- it's too
      big to run through the JS minifier in ResourceLoader, so is loaded
      as a separate payload.
    * Many iOS devices will be too slow to decode video, this is not checked yet
    * Audio support is not checked for
    * all-Flash fallback for older browsers not yet packaged
    * WebGL acceleration not yet used when available
    
    Bug: 61823
    Change-Id: I1a82378281434927f688a04095dbf5bfbeca7025

--- MwEmbedModules/EmbedPlayer/EmbedPlayer.php
+++ MwEmbedModules/EmbedPlayer/EmbedPlayer.php
@@ -68,6 +68,7 @@
 				'scripts'=> "resources/mw.EmbedPlayerVLCApp.js",
 				'dependencies' => array( 'mediawiki.Uri' )
 			),
+			"mw.EmbedPlayerOgvJs"	=> array( 'scripts'=> "resources/mw.EmbedPlayerOgvJs.js" ),
 			"mw.EmbedPlayerImageOverlay" => array( 'scripts'=> "resources/mw.EmbedPlayerImageOverlay.js" ),
 
 			"mw.EmbedPlayerVlc" => array( 'scripts'=> "resources/mw.EmbedPlayerVlc.js" ),
