0e3dde955863e5e89d0e872ed6b96a096a7a2c6f
Brion Vibber <brion@pobox.com>
2014-10-08 06:24:58 -0700

    Work in progress: ogv.js media player for Safari/IE (2 of 3)
    Plugin to MwEmbedPlayer for using ogv.js JavaScript playback
    of Ogg Theora/Vorbis media in Safari and IE.
    
    The actual JS libraries are in the previous commit in order
    to make this commit easier to review.
    
    This commit adds only the plugin for the desktop player and
    support code that's common with the mobile player coming in
    the next commit.
    
    Bug: T63823
    Change-Id: Ia6b2e66864a596dbb4ba5841b9da135021f38276

--- TimedMediaHandler.hooks.php
+++ TimedMediaHandler.hooks.php
@@ -91,6 +91,21 @@ class TimedMediaHandlerHooks {
 				'loaderScripts' => 'resources/mw.MediaWikiPlayer.loader.js',
 			),
 		);
+
+		// Add OgvJs-related modules for Safari/IE Ogg playback
+		$wgResourceModules += array(
+			'ext.tmh.bogoslow' => $baseExtensionResource + array(
+				'scripts' => 'resources/ext.tmh.bogoslow.js',
+			),
+			'ext.tmh.OgvJsSupport' => $baseExtensionResource + array(
+				'scripts' => array(
+					'MwEmbedModules/EmbedPlayer/binPlayers/ogv.js/ogv-version.js',
+					'resources/ext.tmh.OgvJsSupport.js',
+				),
+				'targets' => array( 'mobile', 'desktop' ),
+			),
+		);
+
 		// Setup a hook for iframe embed handling:
 		$wgHooks['ArticleFromTitle'][] = 'TimedMediaIframeOutput::iframeHook';
 
