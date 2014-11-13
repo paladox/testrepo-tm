f7fcd2c2006e5841ee8107b132171f556aea85fe
Brion Vibber <brion@pobox.com>
2014-11-12 23:12:56 -0800

    User-friendly prompt to install WebM components for Internet Explorer
    Instead of giving a raw-file download prompt, link directly to Google's IE WebM
    plugin download page with some brief instructions. After installing, reload the
    page and *poof* the video works.
    
    Notes:
    * Components are not available for Windows RT (Windows on ARM), so the download is
      not offered in this case.
    * Components only work with IE 9 and above, but since the player framework doesn't
      seem to work on IE 8 and below anyway we don't bother to check versions here.
    * These really are WebM only, and do not include support for Ogg audio files.
    * In future, ogv.js should cover the audio case and low-resolution video case without
      the plugin prompt, but we'll still need WebM for HD video playback.
    
    Bug: 73348
    Change-Id: I2ad8257d8752871b67632dc7b7b42d2825f8fc68

--- MwEmbedModules/EmbedPlayer/resources/mw.MediaPlayers.js
+++ MwEmbedModules/EmbedPlayer/resources/mw.MediaPlayers.js
@@ -34,7 +34,7 @@ mw.MediaPlayers.prototype = {
 		this.defaultPlayers['application/vnd.apple.mpegurl'] = ['Native'];
 
 		this.defaultPlayers['video/ogg'] = ['Native', 'Vlc', 'Java', 'Generic', 'VLCApp'];
-		this.defaultPlayers['video/webm'] = ['Native', 'Vlc', 'VLCApp'];
+		this.defaultPlayers['video/webm'] = ['Native', 'Vlc', 'VLCApp', 'IEWebMPrompt'];
 		this.defaultPlayers['application/ogg'] = ['Native', 'Vlc', 'Java', 'Generic', 'VLCApp'];
 		this.defaultPlayers['audio/ogg'] = ['Native', 'Vlc', 'Java', 'VLCApp'];
 		this.defaultPlayers['audio/mpeg']= ['Native', 'Kplayer'];
