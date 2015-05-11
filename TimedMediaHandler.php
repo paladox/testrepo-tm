8b2a4e4d6007d7bb0ce2a770e8053ecc13457076
paladox <thomasmulhall410@yahoo.com>
2015-05-11 21:52:35 +0200

    Add support for 4K and 8K videos
    * Supports 4k videos at resolution 4096x2160.
    * Supports 8K videos at resolution 7680x4320.
    
    Bug: T98694
    Change-Id: I65c59bc3b93276b9764081d37aa1de8ea96aebf9

--- TimedMediaHandler.php
+++ TimedMediaHandler.php
@@ -55,7 +55,7 @@ $wgWaitTimeForTranscodeReset = 3600;
 $wgMinimumVideoPlayerSize = 200;
 
 // Set the supported ogg codecs:
-$wgMediaVideoTypes = array( 'Theora', 'VP8' );
+$wgMediaVideoTypes = array( 'Theora', 'VP8', 'VP9' );
 $wgMediaAudioTypes = array( 'Vorbis', 'Speex', 'FLAC', 'Opus' );
 
 // Default skin for mwEmbed player
@@ -143,6 +143,12 @@ $wgEnabledTranscodeSet = array(
 	// A full-HD high quality WebM stream
 	WebVideoTranscode::ENC_WEBM_1080P,
 /*
+	// A 4K full high quality WebM stream
+	WebVideoTranscode::ENC_WEBM_2160P,
+
+	// A 8K full high quality WebM stream
+	WebVideoTranscode::ENC_WEBM_4320P,
+
 	// A least common denominator h.264 stream; first gen iPhone, iPods, early android etc.
 	WebVideoTranscode::ENC_H264_320P,
 
@@ -154,6 +160,12 @@ $wgEnabledTranscodeSet = array(
 
 	// A full-HD high quality stream; higher end phones, tablets, smart tvs
 	WebVideoTranscode::ENC_H264_1080P,
+
+	// A 4K high quality stream; higher end phones, tablets, smart tvs
+	WebVideoTranscode::ENC_H264_2160P,
+
+	// A 8K high quality stream; higher end phones, tablets, smart tvs
+	WebVideoTranscode::ENC_H264_4320P,
 */
 );
 
