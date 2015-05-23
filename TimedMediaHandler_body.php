e4cb3df42c71aca102b7db6dc1655862a8c1f5f1
Derk-Jan Hartman <hartman.wiki@gmail.com>
2015-05-23 20:23:42 +0200

    [WIP] Using VideoJS instead of Kaltura player
    Bug: T100106
    Change-Id: I0a0b7a1727a3431fb6b924ee379b4cc1226f6547

--- TimedMediaHandler_body.php
+++ TimedMediaHandler_body.php
@@ -203,9 +203,6 @@ class TimedMediaHandler extends MediaHandler {
 	/**
 	 * Parser output hook only adds the PopUpMediaTransform
 	 *
-	 * The core embedPlayer module is part of a "loaderScript" so it does not need to
-	 * be registered here.
-	 *
 	 * TODO move core loader to on-page script as to not include it on all pages.
 	 *
 	 * @param $outputPage OutputPage
@@ -213,14 +210,8 @@ class TimedMediaHandler extends MediaHandler {
 	 * @param $data
 	 */
 	static function outputHook( $outputPage, $parserOutput, $data ) {
-		// Add the PopUpMediaTransform code
-		$outputPage->addModuleScripts( 'mw.PopUpMediaTransform' );
-		$outputPage->addModuleStyles( 'mw.PopUpMediaTransform.styles' );
-		$outputPage->addModules( 'mw.TMHGalleryHook.js' );
-		if ( $parserOutput ) {
-			// Not present when run from outputpage hooks, like File/Category etc...
-			$parserOutput->setExtensionData( 'mw_ext_TMH_hasTimedMediaTransform', true );
-		}
+		// TODO add modules that pages with video need
+		$outputPage->addModules( 'ext.tmh.player' );
 	}
 
 	/**
