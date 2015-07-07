45f733a693999f0614f67cdbb3ca32a1de58001d
Brion Vibber <brion@pobox.com>
2015-07-07 03:23:30 -0700

    WIP: very basic TimedMediaHandler frontend for mobile
    Doesn't try to integrate much into MobileFrontend's routing/overlay
    system; in fact should have no specific dependency on MobileFrontend.
    
    'Popup transforms' are handled here, so instead of a thumbnail and
    a link to the high-resolution original, you get a thumbnail with
    a play link, and the actual audio/video inline.
    
    (Audio files, and videos that were not originally squished into a
    popup transform, should continue to appear as they were.)
    
    Audio/video files playback inline by default on Android (native),
    iOS (ogv.js), and Windows 10 Mobile (ogv.js).
    
    Todo:
    * ogv.js currently has no native controls; coming soon
    * fix invisible audio with ogv.js
    * track down moar bugs
    
    Task: T62565
    Change-Id: I1a108f7e085c97e0cf309dad88f8c657796dd9c3

---
+++ resources/ext.tmh.mobile.js
@@ -0,0 +1,177 @@
+( function( $, mw, OGVCompat ) {
+
+	// Globalish state for the lightweight mobile media player
+	var Mtmh = {
+		autoplay: false,
+		audioContext: null,
+		initAudioContext: function() {
+			if ( !Mtmh.audioContext ) {
+				Mtmh.audioContext = mw.OgvJsSupport.initAudioContext();
+			}
+			return Mtmh.audioContext;
+		}
+	};
+
+	/**
+	 * Find the hidden audio or video element in the container!
+	 * Beware it may be hiding in an attribute.
+	 *
+	 * @return jQuery containing 0 or 1 media elements
+	 */
+	function findMediaPlayer( $container ) {
+		var payload = $container.attr( 'videopayload' );
+		if ( payload ) {
+			$container = $( payload );
+		}
+		if ( $container.is( 'audio, video' ) ) {
+			return $container;
+		} else {
+			return $container.find( 'audio, video' );
+		}
+	}
+
+	function autoplayClick( id ) {
+		// Use a single audio context for all players,
+		// and pre-initialize it from event handler on iOS
+		// to work around the restrictions on auto playback.
+		Mtmh.initAudioContext();
+		Mtmh.autoplay = true;
+
+		var $container = $( '#' + id ),
+			$player = findMediaPlayer( $container );
+
+		if ( $player.length ) {
+			var player = $player[0];
+
+			transformMediaElement( player ).then( function( el ) {
+				var w = $container.width(),
+					h = $container.height();
+				if ( !w ) {
+					w = 220;
+				}
+				if ( !h ) {
+					h = 26;
+				}
+				$( el )
+					.css( 'width', w + 'px' )
+					.css( 'height', h + 'px' )
+					.replaceAll( $container );
+				el.play();
+			} );
+		}
+	}
+
+	function transformMediaPopups( container ) {
+		var $container = $( container );
+
+		// Transform the target URL to use our magic player
+		$container.find( '.PopUpMediaTransform' ).each( function( i, el ) {
+			var $popupTransform = $( el ),
+				id = $popupTransform.attr( 'id' );
+			$popupTransform
+				.find( 'a' )
+					.attr( 'target', '' )
+					.off()
+					.on( 'click', function( ev ) {
+						autoplayClick( id );
+						ev.preventDefault();
+					} )
+					.end();
+		} );
+
+	}
+
+	/**
+	 * Transform any live media elements in the given container
+	 * into ogv.js elements.
+	 *
+	 * May happen asynchronously. Whee!
+	 *
+	 * @param HTMLElement container
+	 */
+	function transformMediaElements( container ) {
+		var $media = $( container ).find( 'audio, video' );
+		$media.each( function( i, el ) {
+			transformMediaElement( el );
+		} );
+	}
+	
+	/**
+	 * Transform a single media element into an ogv.js player if needed.
+	 * @param el HTMLMediaElement target element
+	 * @return promise
+	 */
+	function transformMediaElement( el ) {
+		return $.Deferred( function( deferred ) {
+			var oga = /^audio\/ogg;\s*codecs="(vorbis|opus)"$/,
+				ogv = /^video\/ogg;\s*codecs="theora(,\s*(vorbis|opus))?"$/,
+				nativeSources = 0,
+				oggSources = 0,
+				preferredSource = null,
+				preferredSourceHeight = 0,
+				supportsOgvJs = OGVCompat.supported( 'OGVPlayer' ),
+				maxJsHeight = (OGVCompat.isSlow() ? 160 : 360);
+			$( el ).find( 'source' ).each( function( j, source ) {
+				var type = source.type;
+				if ( el.canPlayType( type ) ) {
+					nativeSources++;
+				} else if ( supportsOgvJs ) {
+					if ( type.match( oga ) || type.match( ogv ) ) {
+						oggSources++;
+						var height = $( source ).data( 'height' );
+						if ( ( preferredSource === null || height > preferredSourceHeight ) && height <= maxJsHeight ) {
+							preferredSource = source;
+							preferredSourceHeight = height;
+						}
+					}
+				}
+			} );
+			if ( oggSources > 0 && nativeSources === 0 ) {
+				mw.OgvJsSupport.loadOgvJs().then( function() {
+					if ( typeof OGVPlayer === 'undefined' ) {
+						// failed to load ogv.js
+						deferred.resolve( el );
+					} else {
+						var player = createPlayer();
+						
+						player.src = preferredSource.src;
+						player.poster = el.poster;
+						$( player ).on( 'click', function() {
+							if ( player.paused ) {
+								player.play();
+							} else {
+								player.pause();
+							}
+						} );
+						
+						$( el ).replaceWith( player );
+						deferred.resolve( player );
+					}
+				} );
+			} else {
+				deferred.resolve( el );
+			}
+		} );
+	}
+
+	function createPlayer() {
+		var options = {
+			base: mw.OgvJsSupport.basePath()
+		};
+		if ( Mtmh.audioContext ) {
+			// Reuse the audio context we opened earlier
+			options.audioContext = Mtmh.audioContext;
+		}
+		return new OGVPlayer( options );
+	}
+
+	function prepTransforms( $container ) {
+		transformMediaPopups( $container );
+		transformMediaElements( $container );
+	}
+
+	$( function() {
+		prepTransforms( $( '#bodyContent' ) );
+	} );
+
+} )( jQuery, mediaWiki, OGVCompat );
