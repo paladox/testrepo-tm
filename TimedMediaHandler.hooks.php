aeb14c2c28318bdf06e499acb6c3e8ddfff82ad5
Derk-Jan Hartman <hartman.wiki@gmail.com>
2015-08-17 23:38:32 +0200

    [WIP] First go at a subtitle serving api
    Will serve up subtitle files for <track> elements and possibly other
    purposes.
    
    https://www.mediawiki.org/wiki/Extension:TimedMediaHandler/TimedTextRework
    
    Bug: T111552
    Change-Id: Ib61af0be9f3d5911bcfb5637411b236cb9f18991

--- TimedMediaHandler.hooks.php
+++ TimedMediaHandler.hooks.php
@@ -16,6 +16,10 @@ class TimedMediaHandlerHooks {
 		$wgExtensionAssetsPath, $wgMwEmbedModuleConfig, $timedMediaDir,
 		$wgEnableLocalTimedText, $wgTmhFileExtensions;
 
+		if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+			require_once __DIR__ . '/vendor/autoload.php';
+		}
+
 		// Remove mp4 if not enabled:
 		if( $wgTmhEnableMp4Uploads === false ){
 			$index = array_search( 'mp4', $wgFileExtensions );
