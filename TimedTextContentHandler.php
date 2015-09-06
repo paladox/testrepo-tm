90078e0a3bb6b926984f4a2cda8d0be240600568
Derk-Jan Hartman <hartman.wiki@gmail.com>
2015-09-06 22:29:27 +0200

    [WIP] TimedText should use ContentHandler
    Bug: T111651
    Change-Id: I8cb5e60cbfed27296fbb093694e91b8e77669729

---
+++ TimedTextContentHandler.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Created on Sep 5, 2015
+ *
+ * Copyright © 2015 Derk-Jan Hartman "hartman.wiki@gmail.com"
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * Timed Text Content Handler
+ *
+ * @file
+ * @ingroup Extensions
+ * @ingroup TimedText
+ * @author Derk-Jan Hartman <hartman.wiki@gmail.com>
+ * @since 1.27
+ */
+namespace TimedMediaHandler\TimedText;
+
+use Content;
+use WikitextContentHandler;
+use Title;
+
+class ContentHandler extends WikitextContentHandler {
+	static private $knownTimedTextExtensions = [ 'srt', 'vtt' ];
+
+	/**
+	 * @param string $modelId
+	 * @param string[] $formats
+	 */
+	public function __construct(
+		$modelId = CONTENT_MODEL_TIMEDTEXT, $formats = [ CONTENT_FORMAT_WIKITEXT ]
+	) {
+		parent::__construct( $modelId, $formats );
+	}
+
+	protected function getContentClass() {
+		return \TimedMediaHandler\TimedText\Content::class;
+	}
+
+	/**
+	 * Only allow this content handler to be used in the TimedText namespace
+	 *
+	 * @param Title $title
+	 * @return bool
+	 */
+	public function canBeUsedOn( Title $title ) {
+		if ( $title->getNamespace() !== NS_TIMEDTEXT ) {
+			return false;
+		}
+		if ( self::getFormatForTitle( $title ) === null ) {
+			return false;
+		}
+
+		return parent::canBeUsedOn( $title );
+	}
+
+	/**
+	 * Subtitle language is currently defined by the format of the title
+	 *
+	 * @param Title $title
+	 * @param Content $content
+	 * @return Language
+	 */
+	public function getPageLanguage( Title $title, Content $content = null ) {
+		return self::getLanguageForTitle( $title );
+	}
+
+	/**
+	 * Subtitle language is currently defined by the format of the title
+	 *
+	 * @param Title $title
+	 * @param Content $content
+	 * @return Language
+	 */
+	public function getPageViewLanguage( Title $title, Content $content = null ) {
+		return self::getLanguageForTitle( $title );
+	}
+
+	public function getActionOverrides() {
+		return [
+			'view' => '\TimedMediaHandler\TimedText\PageViewAction',
+			// 'edit' => '\TimedMediaHandler\TimedText\EditAction',
+			// 'submit' => '\TimedMediaHandler\TimedText\SubmitAction',
+		];
+	}
+
+	/**
+	 * @see ContentHandler::makeParserOptions
+	 */
+	public function makeParserOptions( $context ) {
+		$parserOptions = parent::makeParserOptions( $context );
+		$parserOptions->setEditSection( false );
+		return $parserOptions;
+	}
+
+	private function getLanguageForTitle( Title $title ) {
+		$titleParts = explode( '.', $title->getDBkey() );
+		$timedTextExtension = array_pop( $titleParts );
+		$language = array_pop( $titleParts );
+		return wfGetLangObj( $language );
+	}
+
+	private function getFormatForTitle( Title $title ) {
+		$titleParts = explode( '.', $title->getDBkey() );
+		$timedTextExtension = array_pop( $titleParts );
+		if ( in_array( $timedTextExtension, self::$knownTimedTextExtensions ) ) {
+			return $timedTextExtension;
+		}
+		return null;
+	}
+
+	private function getMimeType( Title $title, Content $content = null ) {
+		$timedTextExtension = self::getFormatForTitle( $title );
+		if ( $timedTextExtension === 'srt' ) {
+			return 'text/x-srt';
+		} elseif ( $timedTextExtension === 'vtt' ) {
+			return 'text/vtt';
+		}
+		return null;
+	}
+}
--- TimedTextViewAction.php
+++ TimedTextViewAction.php
@@ -1,53 +1,78 @@
 <?php
 /**
- * TimedText page display the current video with subtitles to the right.
- * @deprecated
+ * Created on April 21, 2016
+ *
+ * Copyright © 2015 Derk-Jan Hartman "hartman.wiki@gmail.com"
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * Timed Text Page view action for the TimedText contentmodel
+ * Displays the current video with subtitles to the right.
+ *
+ * @file
+ * @ingroup Extensions
+ * @ingroup TimedText
+ * @author Derk-Jan Hartman <hartman.wiki@gmail.com>
+ * @since 1.27
  */
-class TimedTextPage extends Article {
+namespace TimedMediaHandler\TimedText;
+
+use ViewAction;
+use Title;
+use Revision;
+use Language;
+use Xml;
+
+class PageViewAction extends ViewAction {
 	// The width of the video plane:
 	static private $videoWidth = 400;
+	// TODO need to move to to a less specific class
 	static private $knownTimedTextExtensions = [ 'srt', 'vtt' ];
 
-	public function view() {
-		$request = $this->getContext()->getRequest();
-		$out = $this->getContext()->getOutput();
-		$user = $this->getContext()->getUser();
-
-		$diff = $request->getVal( 'diff' );
-
-		if ( $this->getTitle()->getNamespace() != NS_TIMEDTEXT || isset( $diff ) ) {
-			parent::view();
+	public function show() {
+		global $wgTimedTextNS;
+		$out = $this->getOutput();
+		$title = $this->page->getTitle();
+		// TODO handle the preview of the content of the diff
+		if ( !$title->inNamespace( $wgTimedTextNS )
+			|| $out->isPrintable()
+			|| $this->getContext()->getRequest()->getCheck( 'diff' )
+		) {
+			$this->page->view();
 			return;
 		}
-		$this->renderOutput( $out );
-	}
-
-	/**
-	 * Render TimedText to given output
-	 * @param $out OutputPage
-	 */
-	public function renderOutput( $out ) {
+		$wikiPage = $this->page->getPage();
+		$content = $wikiPage->getContent( Revision::FOR_THIS_USER, $this->getUser() );
+		if ( $content === null
+			|| $content->getModel() !== CONTENT_MODEL_TIMEDTEXT
+			|| $content->isRedirect()
+		) {
+			$this->page->view();
+			return;
+		}
 		// parse page title:
-		$titleParts = explode( '.', $this->getTitle()->getDBkey() );
+		$titleParts = explode( '.', $title->getDBkey() );
 		$timedTextExtension = array_pop( $titleParts );
 		$languageKey = array_pop( $titleParts );
 
-		$oldid = $this->getOldID();
-		# Are we looking at an old revision
-		if ( $oldid && $this->mRevision ) {
-			$this->fetchContentObject();
-			$out->setRevisionId( $this->getRevIdFetched() );
-			$this->setOldSubtitle( $oldid );
-
-			if ( !$this->showDeletedRevisionHeader() ) {
-				wfDebug( __METHOD__ . ": cannot view deleted revision\n" );
-				return;
-			}
-		}
+		// Our custom view
 
 		// Check for File name without text extension:
 		// i.e TimedText:myfile.ogg
-		$fileTitle = Title::newFromText( $this->getTitle()->getDBkey(), NS_FILE );
+		$fileTitle = Title::newFromText( $title->getDBkey(), NS_FILE );
 		$file = wfFindFile( $fileTitle );
 		// Check for a valid srt page, present redirect form for the full title match:
 		if ( !in_array( $timedTextExtension, self::$knownTimedTextExtensions ) &&
@@ -88,12 +113,13 @@ class TimedTextPage extends Article {
 		}
 
 		// Set title
-		$message = $this->exists() ?
+		$message = $this->page->exists() ?
 			'mwe-timedtext-language-subtitles-for-clip' :
 			'mwe-timedtext-language-no-subtitles-for-clip';
 		$out->setPageTitle( wfMessage( $message, $languageName, $videoTitle ) );
 
 		// Get the video with with a max of 600 pixel page
+		// TODO get rid of table layout here
 		$out->addHTML(
 			xml::tags( 'table', [ 'style'=> 'border:none' ],
 				xml::tags( 'tr', null,
@@ -127,7 +153,7 @@ class TimedTextPage extends Article {
 
 		// Look up the language name:
 		$language = $out->getLanguage()->getCode();
-		$attrs = [ 'id' => 'timedmedia-tt-input' ];
+		$attrs = array( 'id' => 'timedmedia-tt-input' );
 		$langSelect = Xml::languageSelector( $language, false, null, $attrs, null );
 
 		$out->addHTML(
@@ -172,14 +198,15 @@ class TimedTextPage extends Article {
 	 * @return Message|string
 	 */
 	private function getTimedTextHTML( $languageName ) {
-		if ( !$this->exists() ) {
+		if ( !$this->page->exists() ) {
 			return wfMessage( 'timedmedia-subtitle-no-subtitles',  $languageName );
 		}
 		return Xml::element(
 			'pre',
 			[ 'style' => 'margin-top: 0px;' ],
-			$this->getContent(),
+			$this->page->getContent(),
 			false
 		);
 	}
+
 }
