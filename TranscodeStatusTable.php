af641b34d1897d2946edb97861f968320c12e4ce
Paladox <thomasmulhall410@yahoo.com>
2015-10-15 21:13:18 +0100

    Add php code sniffer
    Change-Id: I3be13a665e02f57f4a24d0316cafa313f3e2a6a9

--- TranscodeStatusTable.php
+++ TranscodeStatusTable.php
@@ -11,28 +11,31 @@ class TranscodeStatusTable {
 	 * @param $file File
 	 * @return string
 	 */
-	public static function getHTML( $file ){
+	public static function getHTML( $file ) {
 		global $wgUser, $wgOut;
 
 		// Add transcode table css and javascript:
 		$wgOut->addModules( array( 'ext.tmh.transcodetable' ) );
 
-		$o = '<h2 id="transcodestatus">' . wfMessage( 'timedmedia-status-header' )->escaped() . '</h2>';
+		// @codingStandardsIgnoreStart
+		$o= '<h2 id="transcodestatus">' . wfMessage( 'timedmedia-status-header' )->escaped() . '</h2>';
 		// Give the user a purge page link
 		$o.= Linker::link(
 			$file->getTitle(),
-			wfMessage('timedmedia-update-status')->escaped(),
+			wfMessage( 'timedmedia-update-status' )->escaped(),
 			array(),
 			array( 'action'=> 'purge' )
 		);
 
-		$o.= Xml::openElement( 'table', array( 'class' => 'wikitable mw-filepage-transcodestatus' ) ) . "\n"
+		$o.= Xml::openElement( 'table',
+			array( 'class' => 'wikitable mw-filepage-transcodestatus' )
+		) . "\n"
 			. '<tr>'
 			. '<th>' . wfMessage( 'timedmedia-transcodeinfo' )->escaped() . '</th>'
 			. '<th>' . wfMessage( 'timedmedia-transcodebitrate' )->escaped() . '</th>'
 			. '<th>' . wfMessage( 'timedmedia-direct-link' )->escaped() . '</th>';
 
-		if( $wgUser->isAllowed( 'transcode-reset' ) ) {
+		if ( $wgUser->isAllowed( 'transcode-reset' ) ) {
 			$o.= '<th>' . wfMessage( 'timedmedia-actions' )->escaped() . '</th>';
 		}
 
@@ -44,6 +47,7 @@ class TranscodeStatusTable {
 
 		$o.=  Xml::closeElement( 'table' );
 		return $o;
+		// @codingStandardsIgnoreEnd
 	}
 
 	/**
@@ -55,7 +59,7 @@ class TranscodeStatusTable {
 			$settings = WebVideoTranscode::$derivativeSettings[$key];
 			if ( isset( $settings['videoCodec'] ) ) {
 				return $settings['videoCodec'];
-			} else if ( isset( $settings['audioCodec'] ) ) {
+			} elseif ( isset( $settings['audioCodec'] ) ) {
 				return $settings['audioCodec'];
 			} else {
 				// this this shouldn't happen...
@@ -72,9 +76,11 @@ class TranscodeStatusTable {
 	 * @param $file File
 	 * @return string
 	 */
-	public static function getTranscodeRows( $file ){
-		global $wgUser, $wgLang;
-		$o='';
+	public static function getTranscodeRows( $file ) {
+		global $wgUser;
+		// @codingStandardsIgnoreStart
+		$o= '';
+		// @codingStandardsIgnoreEnd
 
 		$transcodeRows = WebVideoTranscode::getTranscodeState( $file );
 
@@ -88,36 +94,37 @@ class TranscodeStatusTable {
 
 			if ( $aIndex === false && $bIndex === false ) {
 				return -strnatcmp( $a, $b );
-			} else if ( $aIndex === false ) {
+			} elseif ( $aIndex === false ) {
 				return 1;
-			} else if ( $bIndex === false ) {
+			} elseif ( $bIndex === false ) {
 				return -1;
-			} else if ( $aIndex === $bIndex ) {
+			} elseif ( $aIndex === $bIndex ) {
 				return -strnatcmp( $a, $b );
 			} else {
 				return ( $aIndex - $bIndex );
 			}
 		} );
 
-		foreach( $transcodeRows as $transcodeKey => $state ){
-			$o.='<tr>';
+		foreach ( $transcodeRows as $transcodeKey => $state ) {
+			// @codingStandardsIgnoreStart
+			$o.= '<tr>';
 			// Encode info:
-			$o.='<td>' . wfMessage('timedmedia-derivative-' . $transcodeKey )->escaped() . '</td>';
-			$o.='<td>' . self::getTranscodeBitrate( $file, $state ) . '</td>';
+			$o.= '<td>' . wfMessage('timedmedia-derivative-' . $transcodeKey )->escaped() . '</td>';
+			$o.= '<td>' . self::getTranscodeBitrate( $file, $state ) . '</td>';
 
 			// Download file
-			$o.='<td>';
+			$o.= '<td>';
 			$o.= ( !is_null( $state['time_success'] ) ) ?
 				'<a href="'.self::getSourceUrl( $file, $transcodeKey ) .'" title="'.wfMessage
 				('timedmedia-download' )->escaped() .'"><div class="download-btn"><span>' .
 				wfMessage('timedmedia-download' )->escaped(). '</span></div></a></td>' :
 				wfMessage('timedmedia-not-ready' )->escaped();
-			$o.='</td>';
+			$o.= '</td>';
 
 			// Check if we should include actions:
-			if( $wgUser->isAllowed( 'transcode-reset' ) ){
+			if ( $wgUser->isAllowed( 'transcode-reset' ) ) {
 				// include reset transcode action buttons
-				$o.='<td class="mw-filepage-transcodereset"><a href="#" data-transcodekey="' .
+				$o.= '<td class="mw-filepage-transcodereset"><a href="#" data-transcodekey="' .
 					htmlspecialchars( $transcodeKey ). '">' . wfMessage('timedmedia-reset')->escaped() .
 					'</a></td>';
 			}
@@ -127,6 +134,7 @@ class TranscodeStatusTable {
 			$o.='<td>' . self::getTranscodeDuration( $file, $state ) . '</td>';
 
 			$o.='</tr>';
+			// @codingStandardsIgnoreEnd
 		}
 		return $o;
 	}
@@ -147,7 +155,7 @@ class TranscodeStatusTable {
 	 */
 	public static function getTranscodeDuration( $file, $state ) {
 		global $wgLang;
-		if( !is_null( $state['time_success'] ) ) {
+		if ( !is_null( $state['time_success'] ) ) {
 			$startTime = wfTimestamp( TS_UNIX, $state['time_startwork'] );
 			$endTime = wfTimestamp( TS_UNIX, $state['time_success'] );
 			$delta = $endTime - $startTime;
@@ -165,7 +173,7 @@ class TranscodeStatusTable {
 	 */
 	public static function getTranscodeBitrate( $file, $state ) {
 		global $wgLang;
-		if( !is_null( $state['time_success'] ) ) {
+		if ( !is_null( $state['time_success'] ) ) {
 			return $wgLang->formatBitrate( $state['final_bitrate'] );
 		} else {
 			return '';
@@ -180,7 +188,7 @@ class TranscodeStatusTable {
 	public static function getStatusMsg( $file, $state ){
 		global $wgContLang;
 		// Check for success:
-		if( !is_null( $state['time_success'] ) ) {
+		if ( !is_null( $state['time_success'] ) ) {
 			return wfMessage( 'timedmedia-completed-on',
 				$wgContLang->timeAndDate( $state[ 'time_success' ] ) )->escaped();
 		}
