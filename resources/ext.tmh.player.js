e0431dcdb9dd052a497590b0758e3f0f547c8c6a
Derk-Jan Hartman <hartman.wiki@gmail.com>
2016-04-24 18:41:21 +0200

    [WIP] OOjs UI based popup for playing small videojs thumbnails
    Bug: T133500
    Change-Id: Id014c4956a125eb3643cfe2420b58fada9c53205

--- resources/ext.tmh.player.js
+++ resources/ext.tmh.player.js
@@ -1,6 +1,5 @@
-
-( function ( $, mw, videojs ) {
-	var globalConfig, audioConfig, playerConfig, $source;
+( function ( $, mw, videojs, OO ) {
+	var globalConfig, audioConfig, playerConfig, $source, windowManager;
 
 	globalConfig = {
 		language: mw.config.get( 'wgUserLanguage' ),
@@ -37,95 +36,114 @@
 		}
 	};
 
-	/*
-	function growPlayer() {
-		// FIXME: fancy transform stuff should be added here :)
-		// Possibly only do this when content renderer sets a certain class ?
-		var targetWidth = 600,
-			player = this,
-			$playerDiv = $( player.el() );
+	function loadSingleVideoPlayer( mediaElement, readyCallback ) {
+		var $mediaElement = $( mediaElement );
 
-		var width = player.width();
-		var enlargeRatio = targetWidth / width;
-		if ( width < targetWidth ) {
-			$playerDiv
-			.addClass( 'vjs-expanded' )
-			.data( {
-				'original-height': player.height(),
-				'original-width': width,
-				style: $playerDiv.attr( 'style' )
-			} );
-			player.height( Math.ceil( player.height() * enlargeRatio ) );
-			player.width( targetWidth );
-			var parentThumb = $playerDiv.closest( '.thumb' );
-			if ( parentThumb.length ) {
-				parentThumb.data( {
-					style: parentThumb.attr( 'style' ),
-					'class': parentThumb.attr( 'class' )
-				} );
-				parentThumb.removeClass( 'thumb tright tleft tnone' ).addClass( 'removedThumb' ).css( { clear: 'both' } );
-				parentThumb.children( '.thumbinner' ).removeClass( 'thumbinner' ).addClass( 'removedInner' ).css( { width: 'auto', height: 'auto', 'margin-left': 'auto', 'margin-right': 'auto' } );
-			}
-			$playerDiv.css( { clear: 'both', 'margin-left': 'auto', 'margin-right': 'auto' } );
+		playerConfig = $.extend( {}, globalConfig );
+		if ( mediaElement.tagName.toLowerCase() === 'audio' ) {
+			// We hide the big play button, show the controlbar with CSS
+			// We remove the fullscreen button
+			playerConfig = $.extend( true, {}, playerConfig, audioConfig );
 		}
+		$mediaElement.find( 'source' ).each( function () {
+			// FIXME would be better if we can configure the plugin to make use of our preferred attributes
+			$source = $( this );
+			$source.attr( 'res', $source.data( 'height' ) );
+			$source.attr( 'label', $source.data( 'shorttitle' ) );
+		} );
 
-	}
-
-	function shrinkPlayer() {
-		var player = this,
-			$playerDiv = $( player.el() );
-		if ( $playerDiv.hasClass( 'vjs-expanded' ) ) {
-			player.width( $playerDiv.data( 'original-width' ) );
-			player.height( $playerDiv.data( 'original-height' ) );
-			$playerDiv.removeClass( 'vjs-expanded' ).attr( { style: $playerDiv.data( 'style' ) } );
-			var parentThumb = $playerDiv.closest( '.removedThumb' );
-			if ( parentThumb.length ) {
-				parentThumb.attr( { style: parentThumb.data( 'style' ), 'class': parentThumb.data( 'class' ) } );
-				parentThumb.children( '.removedInner' ).attr( 'class', 'thumbinner' );
+		// Launch the player
+		videojs( mediaElement, playerConfig ).ready( function () {
+			if ( readyCallback ) {
+				readyCallback( this );
 			}
-		}
+		} );
 	}
-	*/
 
 	/**
 	 * Load video players for a jQuery collection
      */
 	function loadVideoPlayer() {
-		var videoplayer, $videoplayer;
-
 		this.each( function ( index ) {
-			videoplayer = this;
-			$videoplayer = $( this );
-			playerConfig = $.extend( {}, globalConfig );
-			if ( videoplayer.tagName.toLowerCase() === 'audio' ) {
-				// We hide the big play button, show the controlbar with CSS
-				// We remove the fullscreen button
-				playerConfig = $.extend( true, {}, playerConfig, audioConfig );
-			}
-			$( videoplayer ).attr( {
+			$( this ).attr( {
 				/* Don't preload on pages with many videos, like Category pages */
 				preload: ( index < 10 ) ? 'auto' : 'metadata'
-			} ).find( 'source' ).each( function () {
-				// FIXME would be better if we can configure the plugin to make use of our preferred attributes
-				$source = $( this );
-				$source.attr( 'res', $source.data( 'height' ) );
-				$source.attr( 'label', $source.data( 'shorttitle' ) );
 			} );
+			loadSingleVideoPlayer( this );
+		} );
 
-			// Launch the player
-			videojs( videoplayer, playerConfig ).ready( function () {
-				// TODO disabled the 'enlarging' for now. reconsidering alternative presentation modes
-				// this.on( 'play', growPlayer );
-				// this.on( 'ended', shrinkPlayer );
-				/* More custom stuff goes here */
+		// Chainable
+		return this;
+	}
+
+	/**
+	 * Load video players for a jQuery collection
+     */
+	function loadPopupVideoPlayer() {
+		this.each( function () {
+			var $video = $( this );
+			if ( this.tagName === 'audio'
+				|| $video.data( 'player' ) !== 'popup'
+			) {
+				return;
+			}
+			var $newVideo = $video.clone();
+			$newVideo.attr( 'id', null );
+
+			$video.on( 'click', function ( event ) {
+				event.preventDefault();
+				event.stopPropagation();
+
+				// Make the window, can't reuse the previous one, since content is dynamic
+				// Have to figure something out for this.
+				var videoJsDialog = new mw.VideoJsDialog( {
+					size: 'larger',
+					$mediaElement: $newVideo
+				} );
+				// We are not removing this...
+				windowManager.addWindows( [ videoJsDialog ] );
+
+				var loadedPlayer;
+				loadSingleVideoPlayer( $( videoJsDialog.$body ).find( '.video-js' )[0], function ( player ) {
+					loadedPlayer = player;
+					videoJsDialog.setPlayer( player );
+					//player.width( 600 );
+					player.fluid( true );
+					player.play();
+					player.one( 'ended', function () {
+						videoJsDialog.close();
+					} );
+				} );
+
+				// Open the window!
+				windowManager.openWindow( videoJsDialog ).then( function ( opened ) {
+
+					return opened.then( function ( closing ) {
+						loadedPlayer.pause();
+						return closing.then( function () {
+							loadedPlayer.dispose();
+							setTimeout( function () {
+								windowManager.removeWindows( [ 'videoJsDialog' ] );
+							}, 0 );
+						} );
+					} );
+				} );
 			} );
 		} );
 	}
 
 	$.fn.loadVideoPlayer = loadVideoPlayer;
+	$.fn.loadPopupVideoPlayer = loadPopupVideoPlayer;
 
 	mw.hook( 'wikipage.content' ).add( function ( $content ) {
-		$content.find( '.video-js' ).loadVideoPlayer();
+		$content.find( '.video-js:not([data-player="popup"])' ).loadVideoPlayer();
+	} ).add( function ( $content ) {
+		$content.find( '.video-js[data-player="popup"]' ).loadPopupVideoPlayer();
 	} );
 
-} )( jQuery, mediaWiki, videojs );
+	$( function () {
+		// Create and append a window manager, which will open and close the window.
+		windowManager = new OO.ui.WindowManager();
+		$( 'body' ).append( windowManager.$element );
+	} );
+} )( jQuery, mediaWiki, videojs, OO );
