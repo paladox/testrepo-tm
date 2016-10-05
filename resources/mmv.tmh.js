348124ddbee0b47aa6c73f93669bc35136387dbf
Brion Vibber <brion@pobox.com>
2016-10-05 10:36:37 -0700

    WIP: optional integration of TMH video.js into MMV
    Similar to the mwembed popup transform, but not quite. :)
    
    Uses a placeholder <img> that's now MMV-compatible, then
    loads the video payload from an attribute on a wrapper
    and transforms & plays it.
    
    Todo basics:
    * figure out when to not autoplay ?
    * fix the play in ogv.js -- it doesn't initialize right?
    * fix for aspect ratio too wide for visible area
    * figure out how to integrate with the mobile version
    
    Todo further:
    * extend download feature to download videos instead of stills
    * extend embed feature to embed video instead of still
    
    Todo still further:
    * extend syntax to allow for making video players inline by choice
    
    Bug: T77147
    Change-Id: I2343d913936d5b077a56fa9caff1f534dcaf7b75

---
+++ resources/mmv.tmh.js
@@ -0,0 +1,43 @@
+( function ( mw, $ ) {
+
+	$( document ).on( 'mmv-metadata.tmh', function ( e ) {
+		var formats = [ 'ogg', 'oga', 'ogv', 'ogm', 'webm', 'mp4', 'm4a', 'm4v' ];
+		var extension = e.image.filePageTitle.ext;
+		if ( $.inArray( extension, formats ) !== -1 ) {
+			// do stuff
+			console.log( 'e.viewer', e.viewer );
+			console.log( 'e.image', e.image );
+			console.log( 'e.imageInfo', e.imageInfo );
+			/* viewer.ui.panel.progressBar; */
+
+			var viewer = e.viewer,
+				image = e.image,
+				$container = viewer.ui.canvas.$imageDiv;
+
+			var $placeholder = $( image.thumbnail ).closest( '.media-placeholder' ),
+				payload = $placeholder.attr( 'videopayload' ),
+				$payload = $( payload );
+
+			$container
+				.empty()
+				.append( $payload )
+				.find( 'video,audio' )
+				.loadVideoPlayer();
+
+			var id = $container.find( 'div.video-js' ).attr( 'id' );
+			if ( id ) {
+				viewer.ui.panel.progressBar.animateTo( 25 );
+
+				var player = videojs( id );
+				player.ready( function () {
+					viewer.ui.panel.progressBar.hide();
+
+					// @TODO only play when explicit click?
+					// @FIXME this is not working with ogv.js in Safari
+					player.play();
+				} );
+			}
+		}
+	} );
+
+}( mediaWiki, jQuery ) );
